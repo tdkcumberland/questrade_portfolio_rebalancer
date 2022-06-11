1775168f49cd504eb41ff0cbf40a0a98de25d202
Timothy Cumberland <tdkcumberland@gmail.com>
2022-06-11 12:39:05 -0400

    cash injection feature added

--- portfolio_overview.py
+++ portfolio_overview.py
@@ -1,5 +1,5 @@
 from questrade_helper import Portfolio
 
-OUTPUT = Portfolio(account_type='TFSA', cash_injection=4000.00, cash_injection_cad=True)
+OUTPUT = Portfolio(account_type='RRSP', cash_injection=0.00, cash_injection_cad=True)
 
 print(OUTPUT.final_output.T)
