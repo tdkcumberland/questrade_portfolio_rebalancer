02a0b6b4c9004cd8eaae11178b839ad802312eaf
Timothy Cumberland <tdkcumberland@gmail.com>
2022-06-18 11:46:05 -0400

    turned it into an CLI

--- portfolio_overview.py
+++ portfolio_overview.py
@@ -1,5 +1,14 @@
 from questrade_helper import Portfolio
+import argparse
 
-OUTPUT = Portfolio(account_type='RRSP', cash_injection=0.00, cash_injection_cad=True)
+def main():
+    parser = argparse.ArgumentParser(description="Retrieve a QT account by type then calculates the target composition delta and provide automatic balancing figures")
+    parser.add_argument('-t', '--type', dest="account_type", type=str, help="define account type")
+    parser.add_argument('-c', '--cash', dest="cash_injection", type=float, help="define the additional cash injection on top of what's available in the account", default=0.0 )
+    args = parser.parse_args()
 
-print(OUTPUT.final_output.T)
+    OUTPUT = Portfolio(account_type=args.account_type, cash_injection=args.cash_injection, cash_injection_cad=True)
+    print(OUTPUT.final_output.T)
+
+if __name__=="__main__":
+    main()
