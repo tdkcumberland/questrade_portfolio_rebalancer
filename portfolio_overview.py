2dc6c65b9ecf44bd38c58c58b2076bf8a7fb5987
Timothy Cumberland <tdkcumberland@gmail.com>
2023-03-06 22:46:26 -0500

    added symbol search function

--- portfolio_overview.py
+++ portfolio_overview.py
@@ -1,4 +1,5 @@
 from questrade_helper import Portfolio
+from symbol_search import Symbol
 import argparse
 
 def main():
@@ -7,11 +8,17 @@ def main():
     parser.add_argument('-c', '--cash', dest="cash_injection", type=float, help="define the additional cash injection on top of what's available in the account", default=0.0 )
     parser.add_argument('-e', '--exchange', dest="cash_injection_cad", type=str2bool, help="define what the cash injection's currency is, true = CAD, false = USD", default=True )
     parser.add_argument('-r', '--refresh-token', dest='refresh_token', type=str, help="refresh token for API")
+    parser.add_argument('-s', '--symbol', dest="symbol_search", type=str, help="symbol or ticker to perform a search")
     args = parser.parse_args()
 
-    OUTPUT = Portfolio(account_type=args.account_type, cash_injection=args.cash_injection, cash_injection_cad=args.cash_injection_cad, refresh_token=args.refresh_token)
-    print(OUTPUT.final_output.T)
-    print("Current exchange rate USD to CAD: {:.4f}".format(OUTPUT.exchange_rate_USD_CAD))
+    if args.account_type and not args.symbol_search:
+        OUTPUT = Portfolio(account_type=args.account_type, cash_injection=args.cash_injection, cash_injection_cad=args.cash_injection_cad, refresh_token=args.refresh_token)
+        print(OUTPUT.final_output.T)
+        print("Current exchange rate USD to CAD: {:.4f}".format(OUTPUT.exchange_rate_USD_CAD))
+    elif args.symbol_search and not args.account_type:
+        Symbol(symbol = args.symbol_search, refresh_token = args.refresh_token)
+    else:
+        raise argparse.ArgumentError("Either an account type full portfolio overview or a symbol looking up is required")
 
 def str2bool(value: str):
     if isinstance(value, bool):
--- symbol_search.py
+++ symbol_search.py
@@ -1,12 +1,24 @@
 from questrade_api.questrade import Questrade
 
+import pandas as pd
+
 q = Questrade()
 
 symbol = "PFIX"
 
-search_symbol = q.symbols_search(prefix = symbol, offset = 0)
+class Symbol():
+    def __init__(self, symbol: str, refresh_token: None) -> None:
+        self.questrade_client = Questrade(refresh_token = refresh_token) if refresh_token else Questrade()
+        self.search(symbol=symbol)
 
-if search_symbol['symbols']:
-    symbol_search_result = q.markets_quote(search_symbol['symbols'][0]['symbolId'])
-else:
-    raise ValueError("The symbol " + symbol + " is not found!!!")
+    def search(self, symbol: str):
+
+        self.search_symbol = q.symbols_search(prefix = symbol, offset = 0)
+
+        if self.search_symbol['symbols']:
+            self.symbol_search_result = q.markets_quote(self.search_symbol['symbols'][0]['symbolId'])
+            print(self.symbol_search_result['quotes'][0])
+            df = pd.DataFrame(self.symbol_search_result['quotes'][0], index = ['symbol'])
+            print(df.T)
+        else:
+            raise ValueError("The symbol " + symbol + " is not found!!!")
